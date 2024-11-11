198921b0f15fbc47f30ad6bd1427bc2c797e4825
Ken-2511 <iwmain@outlook.com>
2024-11-11 11:22:36 -0500

    ...

--- ModelSim/testbench.tcl
+++ ModelSim/testbench.tcl
@@ -11,12 +11,10 @@ if {[file exists ../black.mif]} {
 vlib work;
 
 # compile the Verilog source code in the parent folder
-vlog ../milestone1.v ../vga_adapter/*.v ../utils.v
-# compile the Verilog code of the testbench
-vlog *.v
+vlog ../milestone1.v ../vga_adapter/*.v ../utils.v *.v ../*.v
 # start the Simulator, including some libraries that may be needed
 vsim work.testbench -Lf 220model -Lf altera_mf_ver -Lf verilog
 # show waveforms specified in wave.do
 do wave.do
 # advance the simulation the desired amount of time
-run 3000 ns
+run 100000 ns
