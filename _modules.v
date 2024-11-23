4988e5f5eee975e7f9a21437fd890a34d6dbb6d4
Ken-2511 <IWMAIN@outlook.com>
2024-11-23 15:50:54 -0500

    finished building game logic but still need to debug

--- _modules.v
+++ _modules.v
@@ -12,19 +12,26 @@ module m_game_logic (
     output reg game_over,
     input [7:0] last_key_received
 );
-    //may or may mot use this design
-    reg e_update_player_position, e_wall_collision, e_update_ghost_positions, e_ghost_collision, e_eat_food; //enable flads
+    
+    reg e_update_player_position, e_wall_collision, e_update_ghost_positions, e_ghost_collision, e_eat_food; //enable flags
     reg f_update_player_position, f_wall_collision, f_update_ghost_positions, f_ghost_collision, f_eat_food; //finished flags
     reg [3:0] state, next_state;
+    reg [7:0] score;
 
     parameter update_player_position = 3'b000, wall_collision = 3'b001, update_ghost_positions = 3'b010, ghost_collision = 3'b011, eat_food = 3'b100;
+    parameter [7:0] won_score = 8'b10111100;
 
     //turn it into an FSM
+    initial begin
+        score = 8'b0;
+    end 
+
     always @(posedge clock or negedge resetn) begin
         if (!resetn) begin
+            score <= 8'b0;
             finished <= 0;
             player_x <= 5'b00001;
-            player_y <= 4'b0001;
+            player_y <= 4'b0001; //TODO: FIX THE GHOST COORDINATES
             ghost1_x <= 5'b00101;
             ghost1_y <= 4'b0011;
             ghost2_x <= 5'b00110;
@@ -41,12 +48,12 @@ module m_game_logic (
 
     always @(*) begin 
         case(state)
-            update_player_position:
-                if (f_update_player_position) next_state = wall_collision;
-                else next_state = update_player_position;
             wall_collision:
-                if (f_wall_collision) next_state = update_ghost_positions;
+                if(f_wall_collision) next_state = update_player_position;
                 else next_state = wall_collision;
+            update_player_position:
+                if(f_update_player_position) next_state = update_ghost_positions;
+                else next_state = update_player_position;
             update_ghost_positions:
                 if(f_update_ghost_positions) next_state = ghost_collision;
                 else next_state = update_ghost_positions;
@@ -69,8 +76,8 @@ module m_game_logic (
         e_ghost_collision = 0;
 
         case(state)
-            update_player_position: e_update_player_position = 1;
             wall_collision: e_wall_collision = 1;
+            update_player_position: e_update_player_position = 1;
             update_ghost_positions: e_update_ghost_positions = 1;
             ghost_collision: e_ghost_collision = 1;
             eat_food: e_eat_food = 1;
@@ -80,10 +87,11 @@ module m_game_logic (
 
     //player 
     wire [2:0] w, direction;
-    get_direction GD(last_key_received, hs_enable, direction); //in player_movement.v
+    wire collided;
+    get_direction GD(last_key_received, hs_enable, collided, direction); //in player_movement.v
     movement_FSM MF(clock, resetn, he_enable, w, direction); //in player_movement.v
-    m_update_player_position U1(clock, resetn, e_update_player_position, f_update_player_position, direction, player_x, player_y, hs_enable);
-    //put the wall collision here I guess, it's module U2
+    m_wall_collision U1(clock, resetn, e_wall_collision, f_wall_collision, player_x, player_y, direction, collided);
+    m_update_player_position U2(clock, resetn, e_update_player_position, f_update_player_position, direction, player_x, player_y, hs_enable);
 
     //ghosts
     m_update_ghost_positions U3(clock, resetn, e_update_ghost_positions, f_update_ghost_positions, ghost1_x, ghost1_y, ghost2_x, ghost2_y, ghost3_x, ghost3_y);
@@ -98,7 +106,7 @@ endmodule
 module m_update_player_position(clock, resetn, enable, finished, direction, player_x, player_y, hs_enable);
 
     parameter cbit = 11;
-    parameter up = 3'b001, left = 3'b010, down = 3'b011, right = 3'b100; //TODO: STILL NEED TO DO COLLISIONS
+    parameter still = 3'b000, up = 3'b001, left = 3'b010, down = 3'b011, right = 3'b100; //TODO: STILL NEED TO DO COLLISIONS
 
     // Basic inputs
     input clock, resetn, enable, hs_enable;
@@ -130,8 +138,6 @@ module m_update_player_position(clock, resetn, enable, finished, direction, play
             endcase
 
             finished <= 1;  // Finish immediately when enabled, for testing
-            data <= 3'b010; // Example data value
-            addr <= addr + 1; // Increment address
         end
         else if (finished) begin
             finished <= 0;  // Reset to initial state
@@ -141,7 +147,54 @@ module m_update_player_position(clock, resetn, enable, finished, direction, play
 endmodule
 
 //check collision with walls module
-module m_wall_collision(clock, resetn, enable, player_x, player_y);
+module m_wall_collision(clock, resetn, enable, finished, player_x, player_y, direction, collision);
+    input clock, resetn, enable;
+    input [2:0] direction;
+    input [4:0] player_x;
+    input [3:0] player_y;
+    output reg finished, collision;
+
+    wire [4:0] columns = 5'b11101;
+    wire [3:0] directly_up = player_y - 1;
+    wire [4:0] directly_left = player_x - 1;
+    wire [3:0] directly_down = player_y + 1;
+    wire [4:0] directly_right = player_x + 1;
+
+    parameter still = 3'b000, up = 3'b001, left = 3'b010, down = 3'b011, right = 3'b100;
+    reg [8:0] address_temp;
+    wire q;
+
+    always @(*) begin
+        case(direction)
+            up: adress_temp = directly_up * columns + player_x;
+            left: address_temp = player_y * columns + directly_left;
+            down: address_temp = directly_down * columns + player_x;
+            right: address_temp = player_y * columns + directly_right;
+            default: address_temp = 8'b0;
+        endcase
+    end
+    wire [8:0] address;
+    assign address = address_temp;
+    blocks B(address, clock, 3'b0, 1'b0, q);
+
+    always @(posedge clock) begin
+        if (!resetn) begin
+            finished <= 1'b0;
+            collision <= 1'b0;
+        end
+        else if(enable and direction != still) begin
+            if(q)
+                collision <= 1'b1;
+            else
+                collision <= 1'b0;
+            
+            finished <= 1'b1;
+        end 
+        else begin 
+            finished <= 1'b1; //if player is still, then no collision and exist the module
+            collision <= 1'b0;
+        end 
+    end 
 endmodule
 
 //update ghost position module
@@ -209,15 +262,13 @@ endmodule
 
 // ghost collision detection module
 module m_collision (clock, resetn, enable, finished, player_x, player_y, ghost1_x, ghost1_y, ghost2_x, ghost2_y, ghost3_x, ghost3_y, ghost_collision);
-    input clock,
-    input resetn,
-    input enable,
-    input [4:0] player_x,
-    input [3:0] player_y,
-    input [4:0] ghost1_x, ghost2_x, ghost3_x,
-    input [3:0] ghost1_y, ghost2_y, ghost3_y,
+    input clock, resetn, enable;
+    input [4:0] player_x;
+    input [3:0] player_y;
+    input [4:0] ghost1_x, ghost2_x, ghost3_x;
+    input [3:0] ghost1_y, ghost2_y, ghost3_y;
     output reg finished;
-    output reg ghost_collision
+    output reg ghost_collision;
 
     always @(posedge clock or negedge resetn) begin
         if (!resetn) begin
@@ -239,5 +290,35 @@ module m_collision (clock, resetn, enable, finished, player_x, player_y, ghost1_
 endmodule
 
 //module for eating food
-module m_eat_food(clock, resetn, enable, player_x, player_y, food_x, food_y, food_eaten); //TODO: fix this
+module m_eat_food(clock, resetn, enable, finished, player_x, player_y, score);
+    input clock, resetn, enable;
+    input [4:0] player_x;
+    input [3:0] player_y;
+    output reg finished;
+    output reg [7:0] score;
+
+    wire [4:0] columns = 5'b11101;
+    wire [8:0] address;
+    assign address = player_y * columns + player_x;
+
+    reg wr;
+    wire wren, q;
+    assign wren = wr;
+
+    always @(posedge clock) begin 
+        if(!resetn) begin
+            wr <= 1'b0;
+            finished <= 1'b0;
+        end
+        else if(enable) begin
+            wr <= 1'b0;
+            if(q) begin
+                score <= score + 1;
+                wr <= 1'b1;
+                finished <= 1'b1;
+            end
+        end 
+    end
+
+    food F(address, clock, 1'b0, wren, q);
 endmodule 
