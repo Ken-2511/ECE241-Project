96c4a09b53ca8bb69eac2b38076039ebd619a962
Ken-2511 <iwmain@outlook.com>
2024-11-25 04:38:31 -0500

    ...

--- renderer.v
+++ renderer.v
@@ -34,8 +34,8 @@ module m_renderer (
     wire [11:0] selected_data;
     wire is_ghost;
     // last game x and y
-    reg [4:0] g1_game_x_, g2_game_x_, g3_game_x_;
-    reg [3:0] g1_game_y_, g2_game_y_, g3_game_y_;
+    reg [4:0] pl_game_x_, g1_game_x_, g2_game_x_, g3_game_x_;
+    reg [3:0] pl_game_y_, g1_game_y_, g2_game_y_, g3_game_y_;
 
     // Rendering variables
     reg [4:0] curr_x;       // Current rendering logical coordinates
@@ -45,7 +45,6 @@ module m_renderer (
     reg top_left_corner;    // For indicating whether it is the first time rendering the top left corner
 
     // Rendering target index
-    reg [1:0] render_index; // 0: Player, 1: Ghost 1, 2: Ghost 2, 3: Ghost 3
 
     // State encoding
     parameter IDLE = 3'b000, 
@@ -80,7 +79,7 @@ module m_renderer (
                 // next_state = ERASE; // for testing other states
                 next_state = (VGA_X == 159 && VGA_Y == 119) ? ERASE : DRAW_BG;
             ERASE:
-                next_state = render_index == 3 && dx == 4 && dy == 4 ? DRAW_PLAYER : ERASE;
+                next_state = (dx == 4 && dy == 4) ? DRAW_PLAYER : ERASE;
             DRAW_PLAYER:
                 next_state = (dx == 4 && dy == 4) ? DRAW_GHOST1 : DRAW_PLAYER;
             DRAW_GHOST1:
@@ -110,7 +109,6 @@ module m_renderer (
             VGA_COLOR___ <= 12'hFFF; VGA_COLOR__ <= 12'hFFF; VGA_COLOR_ <= 12'hFFF; VGA_COLOR <= 12'hFFF;
             dx <= 4'b0;
             dy <= 4'b0;
-            render_index <= 2'b0;
             curr_x <= 5'b0;
             curr_y <= 4'b0;
             curr_color <= bg_color;
@@ -126,7 +124,6 @@ module m_renderer (
             case (state)
                 IDLE: begin
                     // Prepare for rendering
-                    render_index <= 0;
                     dx <= 0;
                     dy <= 0;
                     curr_x <= pl_game_x;
@@ -167,30 +164,11 @@ module m_renderer (
                     end else begin
                         dx <= 0;
                         dy <= 0;
-                        if (render_index == 0) begin
-                            render_index <= 1;
-                            curr_x <= pl_game_x;
-                            curr_y <= pl_game_y;
-                        end
-                        else if (render_index < 3) begin
-                            render_index <= render_index + 1;
-                            case (render_index + 1)
-                                1: {curr_x, curr_y} <= {g1_game_x_, g1_game_y_};
-                                2: {curr_x, curr_y} <= {g2_game_x_, g2_game_y_};
-                                3: {curr_x, curr_y} <= {g3_game_x_, g3_game_y_};
-                            endcase
-                        end else begin
-                            render_index <= 0;
-                            g1_game_x_ <= g1_game_x;
-                            g1_game_y_ <= g1_game_y;
-                            g2_game_x_ <= g2_game_x;
-                            g2_game_y_ <= g2_game_y;
-                            g3_game_x_ <= g3_game_x;
-                            g3_game_y_ <= g3_game_y;
-                        end
+                        curr_x <= pl_game_x;
+                        curr_y <= pl_game_y;
                     end
-                    bg_x <= curr_x * 5 + dx;
-                    bg_y <= curr_y * 5 + dy;
+                    bg_x <= pl_game_x_ * 5 + dx;
+                    bg_y <= pl_game_y_ * 5 + dy;
                     VGA_X_ <= bg_x;
                     VGA_Y_ <= bg_y;
                     VGA_X <= VGA_X_;
@@ -210,6 +188,8 @@ module m_renderer (
                     else begin
                         dx <= 0;
                         dy <= 0;
+                        pl_game_x_ <= pl_game_x;
+                        pl_game_y_ <= pl_game_y;
                     end
                     VGA_X <= pl_game_x * 5 + dx;
                     VGA_Y <= pl_game_y * 5 + dy;
@@ -228,6 +208,12 @@ module m_renderer (
                     else begin
                         dx <= 0;
                         dy <= 0;
+                        g1_game_x_ <= g1_game_x;
+                        g1_game_y_ <= g1_game_y;
+                        g2_game_x_ <= g2_game_x;
+                        g2_game_y_ <= g2_game_y;
+                        g3_game_x_ <= g3_game_x;
+                        g3_game_y_ <= g3_game_y;
                     end
                     
                     case (state)
